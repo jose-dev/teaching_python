e0a49d6fa7f59ab1535b504823c5ff5ff70cf13d
Jose Jimenez <jose.jimenez@ocado.com>
2017-08-04 22:29:07 +0100

    hangman game fully implemented

---
+++ src/main/game/hangman/hangman_utils.py
@@ -0,0 +1,119 @@
+import random
+from os import system
+
+#WORDS = ['rojo', 'verde', 'amarillo', 'negro', 'gris', 'azul']
+#WORDS = ['rojo', 'verde', 'amarillo']
+ALLOWED_NUMBER_OF_WRONG_GUESS = 5
+DICTIONARIES = [
+    {"name" : "SPANISH ANIMALS", "file": "resources/spanish_animals.txt"},
+    {"name" : "ENGLISH ANIMALS", "file": "resources/english_animals.txt"}
+]
+
+
+def read_file_with_words(filename):
+    filein = open(filename, 'r')
+    words = []
+    for line in filein.readlines():
+        words.append(line.strip('\n'))
+    return words
+
+def all_words_done(words=None, already_done=[]):
+    return len(words) == len(already_done)
+
+def select_new_random_word(words=None, already_done=[]):
+    possible_words = filter(lambda x: x not in already_done, words)
+    return select_random_word(possible_words)
+
+def select_random_word(words=None):
+    return random.choice(words)
+
+def word_to_letters(word):
+    return list(word)
+
+def letters_to_word(letters):
+    return ''.join(letters)
+
+def hide_word(word):
+    return '.' * len(word)
+
+def word_has_letter(word, letter):
+    return letter in word
+
+
+def place_answered_letter(word_to_guess, answer_so_far, letter_guess):
+    word_to_guess = word_to_letters(word_to_guess)
+    answer_so_far = word_to_letters(answer_so_far)
+    for i in range(len(word_to_guess)):
+        if word_to_guess[i] == letter_guess:
+            answer_so_far[i] = letter_guess
+    return letters_to_word(answer_so_far)
+
+
+def word_has_been_guessed(word_to_guess, answer_so_far):
+    return word_to_guess == answer_so_far
+
+
+def messages_to_player(message=None, talk=False):
+    if talk:
+        system("say {0}".format(message))
+    else:
+        print(message)
+
+
+if __name__ == "__main__":
+
+    messages_to_player("Let's play hangman...")
+
+    # select dictionary to use
+    messages_to_player("Select dictionary")
+    for n in range(len(DICTIONARIES)):
+        messages_to_player("{0} - {1}".format(str(n), DICTIONARIES[n]['name']))
+    selected_dictionary = int(raw_input('Which dictionary you want? Provide number: '))
+    word_dictionary = read_file_with_words(DICTIONARIES[selected_dictionary]['file'])
+    #word_dictionary = read_file_with_words(WORDS)
+
+    words_done = []
+    play_on = True
+    while play_on:
+        number_of_guess_left = ALLOWED_NUMBER_OF_WRONG_GUESS
+        wrong_guess = []
+        word_to_guess = select_new_random_word(words=word_dictionary, already_done=words_done)
+        answer_so_far = hide_word(word_to_guess)
+        words_done.append(word_to_guess)
+        while number_of_guess_left > 0 and not word_has_been_guessed(word_to_guess, answer_so_far):
+            # messages to user
+            messages_to_player("Word to guess: {0}".format(answer_so_far))
+            if len(wrong_guess) > 0:
+                messages_to_player("Letters not in word: {0}".format(letters_to_word(wrong_guess)))
+            messages_to_player("you have {0} chances left".format(str(number_of_guess_left)))
+            letter_guess = raw_input('Enter guess: ')
+            print("\n\n\n")
+
+            # check that the letter is part of word to guess
+            if word_has_letter(word_to_guess, letter_guess):
+                messages_to_player("Well done the letter {0} is in the word".format(letter_guess))
+                answer_so_far = place_answered_letter(word_to_guess, answer_so_far, letter_guess)
+                if word_has_been_guessed(word_to_guess, answer_so_far):
+                    messages_to_player("splendid, you guessed that the word: {0}".format(word_to_guess))
+            else:
+                messages_to_player("the letter {0} is not in the word".format(letter_guess))
+                wrong_guess.extend(letter_guess)
+                number_of_guess_left = number_of_guess_left - 1
+                if number_of_guess_left == 0:
+                    messages_to_player("no more chances my friend, you are dead!")
+                    messages_to_player("The secret word was: {0}".format(word_to_guess))
+            print("\n\n\n")
+
+        # decide whether you want or can keep playing
+        if all_words_done(word_dictionary, words_done):
+            messages_to_player("Game over. No more words left to guess")
+            play_on = False
+        else:
+            message = raw_input("Would you like another game? (yes or no): ")
+            if message == 'no':
+                play_on = False
+                messages_to_player("Bye bye")
+
+
+
+
--- src/test/game/hangman/test_hangman_game.py
+++ src/test/game/hangman/test_hangman_game.py
@@ -1,36 +1,41 @@
-from game.hangman import hangman_game
+from game.hangman import hangman_utils
 import unittest
 
-class HangmanGameTest(unittest.TestCase):
+class HangmanUtilsTest(unittest.TestCase):
+
+    def test_read_file_with_words(self):
+        filename = 'resources/helloworld.txt'
+        expected = ['hello', 'world']
+        self.assertEqual(hangman_utils.read_file_with_words(filename), expected)
 
     def test_word_to_letters(self):
         word = 'hello'
         expected = ['h', 'e', 'l', 'l', 'o']
-        self.assertEqual(hangman_game.word_to_letters(word), expected)
+        self.assertEqual(hangman_utils.word_to_letters(word), expected)
 
 
     def test_letters_to_word(self):
         letters = ['h', 'e', 'l', 'l', 'o']
         expected = 'hello'
-        self.assertEqual(hangman_game.letters_to_word(letters), expected)
+        self.assertEqual(hangman_utils.letters_to_word(letters), expected)
 
 
     def test_hide_word(self):
         word = 'hello'
         expected = '.....'
-        self.assertEqual(hangman_game.hide_word(word), expected)
+        self.assertEqual(hangman_utils.hide_word(word), expected)
 
 
     def test_word_has_letter(self):
         letter = 'o'
         word = 'hello'
-        self.assertTrue(hangman_game.word_has_letter(word, letter))
+        self.assertTrue(hangman_utils.word_has_letter(word, letter))
 
 
     def test_word_does_not_have_letter(self):
         letter = 'i'
         word = 'hello'
-        self.assertFalse(hangman_game.word_has_letter(word, letter))
+        self.assertFalse(hangman_utils.word_has_letter(word, letter))
 
 
     def test_place_answered_letter(self):
@@ -38,35 +43,35 @@ class HangmanGameTest(unittest.TestCase):
         guess = 'h...o'
         letter = 'l'
         expected = 'h.llo'
-        self.assertEqual(hangman_game.place_answered_letter(answer, guess, letter), expected)
+        self.assertEqual(hangman_utils.place_answered_letter(answer, guess, letter), expected)
 
 
     def test_word_has_not_been_guessed_yet(self):
         word_to_guess = 'hello'
         answer_so_far = 'h...o'
-        self.assertFalse(hangman_game.word_has_been_guessed(word_to_guess, answer_so_far))
+        self.assertFalse(hangman_utils.word_has_been_guessed(word_to_guess, answer_so_far))
 
 
     def test_word_has_been_guessed(self):
         word_to_guess = 'hello'
         answer_so_far = 'hello'
-        self.assertTrue(hangman_game.word_has_been_guessed(word_to_guess, answer_so_far))
+        self.assertTrue(hangman_utils.word_has_been_guessed(word_to_guess, answer_so_far))
 
 
     def test_all_words_have_not_been_guessed_yet(self):
         all_words = ['hello', 'world']
         already_done = ['hello']
-        self.assertFalse(hangman_game.all_words_done(all_words, already_done))
+        self.assertFalse(hangman_utils.all_words_done(all_words, already_done))
 
 
     def test_all_words_have_been_guessed(self):
         all_words = ['hello', 'world']
         already_done = ['hello', 'world']
-        self.assertTrue(hangman_game.all_words_done(all_words, already_done))
+        self.assertTrue(hangman_utils.all_words_done(all_words, already_done))
 
 
     def test_select_new_random_word(self):
         all_words = ['hello', 'world']
         already_done = ['hello']
         expected = 'world'
-        self.assertEqual(hangman_game.select_new_random_word(all_words, already_done), expected)
+        self.assertEqual(hangman_utils.select_new_random_word(all_words, already_done), expected)
